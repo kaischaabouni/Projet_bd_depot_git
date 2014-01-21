8bcfbe635c988b20b7b6d4ed40a81bda834b3bb2
qays09 <kais9chaabouni@gmail.com>
2014-01-21 16:46:43 +0100

    triggers vols fret et avion capacite et rayon d action

--- Projet_bd_2014/script_sql/declencheurs.sql
+++ Projet_bd_2014/script_sql/declencheurs.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TRIGGER ContrainteCapaciteFret
+CREATE OR REPLACE TRIGGER ContraintesVolFret
 AFTER INSERT OR UPDATE ON VolsFret
 DECLARE
 	nb Integer;
@@ -10,20 +10,33 @@ BEGIN
 	select count(*) into nb from (select * from AvionsFret a INNER JOIN VolsFret v ON a.NumAvionF  = v.NumAvionF where (a.PoidsMax < v.PoidsMin) );
 	if (nb > 0) then raise_application_error (-20999,'L avion affecté à ce vol ne satisfait pas les contraintes de poids');
 	end if;	
+	
+	select  count(*) into nb from (select * from AvionsFret a, Modele m , VolsFret v where (a.NumAvionF  = v.NumAvionF and a.modele = m.Modele 
+																						and m.RayonAction < v.Distance ));
+	if (nb > 0) then raise_application_error (-20999,'L avion affecté à ce vol ne permet pas d effectuer cette distance');
+	end if;																						
 END;
 /
 
-CREATE OR REPLACE TRIGGER ContrainteCapacitePassagers
+CREATE OR REPLACE TRIGGER ContrainteVolPassagers
 AFTER INSERT OR UPDATE ON VolsPassager
 DECLARE
 	nb Integer;
 BEGIN
-	select count(*) into nb from (select * from AvionsPassagers a INNER JOIN VolsPassager v ON a.NumAvionP  = v.NumAvionP where (a.VolumeMax < v.VolumeMin) );
-	if (nb > 0) then raise_application_error (-20999,'L avion affecté à ce vol ne satisfait pas les contraintes de volume');
+	select count(*) into nb from (select * from AvionsPassagers a INNER JOIN VolsPassager v ON a.NumAvionP  = v.NumAvionP where (a.Nb1Cl < v.Nb1ClMin) );
+	if (nb > 0) then raise_application_error (-20999,'L avion affecté à ce vol ne satisfait pas le nombre min en premiere classe');
 	end if;
 
-	select count(*) into nb from (select * from AvionsPassagers a INNER JOIN VolsPassager v ON a.NumAvionP  = v.NumAvionP where (a.PoidsMax < v.PoidsMin) );
-	if (nb > 0) then raise_application_error (-20999,'L avion affecté à ce vol ne satisfait pas les contraintes de poids');
+	select count(*) into nb from (select * from AvionsPassagers a INNER JOIN VolsPassager v ON a.NumAvionP  = v.NumAvionP where (a.Nb2Cl < v.Nb2ClMin) );
+	if (nb > 0) then raise_application_error (-20999,'L avion affecté à ce vol ne satisfait pas le nombre min en deuxieme classe');
 	end if;	
+	
+	select  count(*) into nb from (select * from AvionsPassagers a, Modele m , VolsPassager v where (a.NumAvionP  = v.NumAvionP and a.modele = m.Modele 
+																						and m.RayonAction < v.Distance ));
+	if (nb > 0) then raise_application_error (-20999,'L avion affecté à ce vol ne permet pas d effectuer cette distance');
+	end if;		
+	
 END;
 /
+
+
--- Projet_bd_2014/script_sql/insertion.sql
+++ Projet_bd_2014/script_sql/insertion.sql
@@ -8,10 +8,33 @@ nomaxvalue;
 
 
 insert into Modele values(1,4,2000 ) ;
+insert into Modele values(2,6,2500 ) ;
+insert into Modele values(3,8,3000 ) ;
+insert into Modele values(4,5,3500 ) ;
+
+
 
 insert into AvionsFret values(1, 1, 2500, 2500 ) ;
 
 insert into  VolsFret values(1,current_date ,'Lyon','Paris',CURRENT_TIMESTAMP, 4,800, 2000, 1500, 1, 'N');
 insert into  VolsFret values(2,current_date ,'Lyon','Paris',CURRENT_TIMESTAMP, 4,800, 2000, 1500, 1, 'N');
+insert into  VolsFret values(3,current_date ,'Lyon','Paris',CURRENT_TIMESTAMP, 4,800, 2000, 1500, 1, 'N');
+-- volume sup
+insert into  VolsFret values(7,current_date ,'Lyon','Paris',CURRENT_TIMESTAMP, 4,800, 3000, 1500, 1, 'N');
+-- poids superieur
+insert into  VolsFret values(8,current_date ,'Lyon','Paris',CURRENT_TIMESTAMP, 4,800, 2000, 3500, 1, 'N');
+-- distance > rayon d action
+insert into  VolsFret values(9,current_date ,'Lyon','Paris',CURRENT_TIMESTAMP, 4,3000, 2000, 2500, 1, 'N');
 
-insert into  VolsFret values(2,current_date ,'Lyon','Paris',CURRENT_TIMESTAMP, 4,800, 3000, 1500, 1, 'N');
+
+insert into AvionsPassagers values(2, 1, 20, 120 ) ;
+
+insert into  VolsPassager values(4,current_date ,'Lyon','Paris',CURRENT_TIMESTAMP, 1,800, 10, 105, 2, 'N');
+insert into  VolsPassager values(5,current_date ,'Lyon','Paris',CURRENT_TIMESTAMP, 1,800, 20, 120, 2, 'N');
+--classe 1 sup 
+insert into  VolsPassager values(10,current_date ,'Lyon','Paris',CURRENT_TIMESTAMP, 1,800, 21, 120, 2, 'N');
+--classe 2 sup
+insert into  VolsPassager values(11,current_date ,'Lyon','Paris',CURRENT_TIMESTAMP, 1,800, 20, 121, 2, 'N');
+-- distance > rayon d action
+insert into  VolsPassager values(12,current_date ,'Lyon','Paris',CURRENT_TIMESTAMP, 1,3000, 20, 120, 2, 'N');
+
