ff95514a6eb620e72618a9dd44319a8ab06dc184
qays09 <kais9chaabouni@gmail.com>
2014-01-22 15:32:46 +0100

    fonctionnalité 4

--- Projet_bd_2014/script_sql/requetes.sql
+++ Projet_bd_2014/script_sql/requetes.sql
@@ -22,13 +22,22 @@ insert into  VolsFret values(vols_seq.NEXTVAL, to_date('10/29/09', 'MM/DD/YY') ,
 
 
 --4--
+--Confirmation de la terminaison d'un vol et mise à jour des informations clients, pilotes, hotesses
 SET AUTOCOMMIT OFF;
 SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
-
 --confirmer la terminaison du vol fret n°7 de la date  10/29/09
 update VolsFret set Termine = 'O' where (NumAvionF = 7 and DateVolF = to_date('10/29/09', 'MM/DD/YY')) ;
---chercher a durée du vol
+--chercher la durée du vol
 select Duree from VolsFret where (NumAvionF = 7 and DateVolF = to_date('10/29/09', 'MM/DD/YY'));
 --mettre a jour les point des clients de ce vol, ajouter la duree d = 200
-update Client c set c.Points = c.Points + 200  where c.
 
+update Client c set c.Points = c.Points + 200  
+	where (c.NumClient in (select r.NumClient from  Reservation r, ResaFret rf 
+		where r.NumResa = rf.NumResa and rf.Numvol = 7 and rf.DateVol = to_date('10/29/09', 'MM/DD/YY'))) ;		
+					
+update Pilotes p set p.NbHeuresVolTotal = p.NbHeuresVolTotal + 200 
+	where (p.NumPersoP in (select aff.NumPersoP from AffectationP aff where (aff.Numvol = 7  and aff.DateVol = to_date('10/29/09', 'MM/DD/YY') )));
+
+update Hotesses h set h.NbHeuresVolTotal = h.NbHeuresVolTotal + 200 
+	where (h.NumPersoH in (select aff.NumPersoH from AffectationH aff where (aff.Numvol = 7  and aff.DateVol = to_date('10/29/09', 'MM/DD/YY') )));
+commit;
