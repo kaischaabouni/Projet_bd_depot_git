2690a8c50dd7da0d46168a54c88839613479a5b2
qays09 <kais9chaabouni@gmail.com>
2014-01-22 12:50:07 +0100

    ajout de requete , continue dans trigger

---
+++ Projet_bd_2014/script_sql/requetes.sql
@@ -0,0 +1,33 @@
+create sequence vols_seq 
+start with 100
+increment by 1 
+nomaxvalue; 
+
+
+
+
+--1--
+--Planification d'un nouveau vol
+--insertion d'un vol pour la date 10/29/09 , de Lyon à Paris
+--chercher un avion disponible ( pas affecté à un vol de date  10/29/09 , 
+select  NumAvionF  from AvionsFret where not exists (select a.NumAvionF from AvionsFret a, VolsFret v where v.NumAvionF = a.NumAvionF and v.DateVolF > )
+
+
+insert into  VolsFret values(vols_seq.NEXTVAL, to_date('10/29/09', 'MM/DD/YY') ,'Lyon','Paris',CURRENT_TIMESTAMP, 200,800, 2000, 1500, 1, 'N');
+
+
+
+
+
+
+
+--4--
+SET AUTOCOMMIT OFF;
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+
+--confirmer la terminaison du vol fret n°7 de la date  10/29/09
+update VolsFret set Termine = 'O' where (NumAvionF = 7 and DateVolF = to_date('10/29/09', 'MM/DD/YY')) ;
+--chercher a durée du vol
+select Duree from VolsFret where (NumAvionF = 7 and DateVolF = to_date('10/29/09', 'MM/DD/YY'));
+--mettre a jour les point des clients de ce vol, ajouter la duree d = 200
+update Client c set c.Points = c.Points + 200  where 
--- Projet_bd_2014/script_sql/test_trigger.sql
+++ Projet_bd_2014/script_sql/test_trigger.sql
@@ -19,11 +19,11 @@ insert into AvionsFret values(1, 1, 2500, 2500 ) ;
 insert into  VolsFret values(1,to_date('10/29/09', 'MM/DD/YY') ,'Lyon','Paris',CURRENT_TIMESTAMP, 4,800, 2000, 1500, 1, 'N');
 insert into  VolsFret values(2,to_date('10/29/09', 'MM/DD/YY') ,'Lyon','Paris',CURRENT_TIMESTAMP, 4,800, 2000, 1500, 1, 'N');
 insert into  VolsFret values(3,to_date('10/29/09', 'MM/DD/YY') ,'Lyon','Paris',CURRENT_TIMESTAMP, 4,800, 2000, 1500, 1, 'N');
--- volume sup
+--!!! volume sup
 insert into  VolsFret values(7,current_date ,'Lyon','Paris',CURRENT_TIMESTAMP, 4,800, 3000, 1500, 1, 'N');
--- poids superieur
+--!!! poids superieur
 insert into  VolsFret values(8,current_date ,'Lyon','Paris',CURRENT_TIMESTAMP, 4,800, 2000, 3500, 1, 'N');
--- distance > rayon d action
+--!!! distance > rayon d action
 insert into  VolsFret values(9,current_date ,'Lyon','Paris',CURRENT_TIMESTAMP, 4,3000, 2000, 2500, 1, 'N');
 
 
@@ -31,11 +31,11 @@ insert into AvionsPassagers values(2, 1, 20, 120 ) ;
 
 insert into  VolsPassager values(4,to_date('10/29/09', 'MM/DD/YY') ,'Lyon','Paris',CURRENT_TIMESTAMP, 1,800, 10, 105, 2, 'N');
 insert into  VolsPassager values(5,to_date('10/29/09', 'MM/DD/YY') ,'Lyon','Paris',CURRENT_TIMESTAMP, 1,800, 20, 120, 2, 'N');
---classe 1 sup 
+--!!!classe 1 sup 
 insert into  VolsPassager values(10,current_date ,'Lyon','Paris',CURRENT_TIMESTAMP, 1,800, 21, 120, 2, 'N');
---classe 2 sup
+--!!!classe 2 sup
 insert into  VolsPassager values(11,current_date ,'Lyon','Paris',CURRENT_TIMESTAMP, 1,800, 20, 121, 2, 'N');
--- distance > rayon d action
+--!!!distance > rayon d action
 insert into  VolsPassager values(12,current_date ,'Lyon','Paris',CURRENT_TIMESTAMP, 1,3000, 20, 120, 2, 'N');
 
 -- tester trigger affectation
@@ -59,13 +59,14 @@ insert into Places values(2,4,1,'hublot');
 insert into Client values(1,'chaabouni','kais',31,'antoine','st martin', 38400, 'France', 'Tunisie', 700);
 insert into Client values(2,'shimabukuro','emmanuel',30,'physique','st martin', 38400, 'France', 'France', 400);
 ----
-insert into Reservation values(1,1,to_date('10/29/09'));
-insert into Reservation values(2,1,to_date('10/29/09'));
-insert into Reservation values(3,2,to_date('10/29/09'));
-insert into Reservation values(4,2,to_date('10/29/09'));
+insert into Reservation values(1,1,to_date('10/29/09', 'MM/DD/YY'));
+insert into Reservation values(2,1,to_date('10/29/09', 'MM/DD/YY'));
+insert into Reservation values(3,2,to_date('10/29/09', 'MM/DD/YY'));
+insert into Reservation values(4,2,to_date('10/29/09', 'MM/DD/YY'));
 ----
-insert into ResaPassager values(4,to_date('10/29/09'),1,1,200);
-insert into ResaPassager values(4,to_date('10/29/09'),2,1,200);
-insert into ResaPassager values(4,to_date('10/29/09'),1,1,200);
-insert into ResaPassager values(4,to_date('10/29/09'),1,1,200);
-
+insert into ResaPassager values(4,to_date('10/29/09', 'MM/DD/YY'),1,1,200);
+insert into ResaPassager values(4,to_date('10/29/09', 'MM/DD/YY'),2,1,200);
+insert into ResaPassager values(4,to_date('10/29/09', 'MM/DD/YY'),3,2,200);
+insert into ResaPassager values(4,to_date('10/29/09', 'MM/DD/YY'),4,3,200);
+----!!!!
+insert into ResaPassager values(4,to_date('10/29/09', 'MM/DD/YY'),5,4,200);
