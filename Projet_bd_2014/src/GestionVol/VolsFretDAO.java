f4a570d740defd23b0054c982855b9a2753a2c52
youben <ysf.benkassem@gmail.com>
2014-01-23 12:19:32 +0100

    j

--- Projet_bd_2014/src/GestionVol/VolsFretDAO.java
+++ Projet_bd_2014/src/GestionVol/VolsFretDAO.java
@@ -1,12 +1,29 @@
 package GestionVol;
 
 import Main.DAO;
+
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class VolsFretDAO extends DAO<VolsFretDAO>{
-
+    
+	ResultSet resultat;
+	
 	public ResultSet ShowList() {
-		return null;
+		
+		try{
+	        Statement requete = cn.createStatement();
+			ResultSet resultat = requete.executeQuery("select * from volfret ....");
+			
+			return resultat;
+			
+		}catch(SQLException e){	
+			System.out.println("ERROR ! \n Code d'erreur"+e.getErrorCode());
+			System.out.println("Message d'erreur : "+e.getMessage());
+		}
+		return resultat;
+			
 	}
 
 	public VolsFretDAO create(VolsFretDAO obj) {
--- Projet_bd_2014/src/GestionVol/VolsPassagerDAO.java
+++ Projet_bd_2014/src/GestionVol/VolsPassagerDAO.java
@@ -1,13 +1,29 @@
 package GestionVol;
 
 import Main.DAO;
+
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class VolsPassagerDAO extends DAO<VolsPassagerDAO>{
-
+	
+    ResultSet resultat;
 	
 	public ResultSet ShowList() {
-		return null;
+		
+		try{
+	        Statement requete = cn.createStatement();
+			ResultSet resultat = requete.executeQuery("select * from volpassager ....");
+			
+			return resultat;
+			
+		}catch(SQLException e){	
+			System.out.println("ERROR ! \n Code d'erreur"+e.getErrorCode());
+			System.out.println("Message d'erreur : "+e.getMessage());
+		}
+		return resultat;
+			
 	}
 
 	public VolsPassagerDAO create(VolsPassagerDAO obj) {
